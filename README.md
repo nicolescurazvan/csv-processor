b52483668f6e78860dc984e48ea37c109c83dd0c
Razvan Nicolescu <nicolescurazvan2002@gmail.com>
2025-10-01 22:23:30 +0300

    Completed CRUD operations

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # csv-processor
-A C++ API to parse and process CSV files.
+
+A simple C++ API to parse and process CSV files. It uses a 2D array of strings
+as data and an array of strings as a header. The class is called `CSVDataFrame`.
+The rows and the columns are counted from 0.
+
+## Functions
+
+* `CSVDataFrame(const char *path)`: constructor (from a file)
+* `CSVDataFrame(CSVDataFrame &frm)`: copy constructor
+* `std::string get(int row, std::string col)`: Returns the field at the row
+`row` and the column `col`.
+* `void change(int row, std::string col, std::string name)`: Changes the field
+located at the row `row` and the column named `col` with the value `name`.
+* `void add_record()`: Adds a record at the end.
+* `void add_record(int pos)`: Adds a record at the row `pos`. 
+* `void delete_record(int pos)`: Deletes the record located at the row `pos`.
+* `void add_column(int pos, std::string name)`: Adds a column at the position 
+`pos` and the name is set to `name`.
+* `void delete_column(int pos)`: Deletes the `pos`-th column.
--- main.cpp
+++ main.cpp
@@ -5,28 +5,35 @@
 #include <string>
 
 // Function to split the string into tokens based on the CSV standard
-std::vector<std::string> split(std::string str, char sep) {
+std::vector<std::string> split(std::string str) {
     std::vector<std::string> tokens;
     std::string tok;
     bool inside_quotas = false;
-    int i, n = str.size();
-    for (i = 0; i < n; i++) {
-        if (str[i] == '\"') {
-            if (str[i + 1] == '\"') {
-                tok.push_back('\"');
-            } else {
-                inside_quotas = !inside_quotas;
-            }
+    int i = 0, n = str.size();
+    while (i < n) {
+        if (str[i] == '\"' && !inside_quotas) {
+            inside_quotas = true;
+            i++;
         }
-        if (str[i] == sep && !inside_quotas) {
+        else if (str[i] == '\"' && inside_quotas && str[i + 1] == '\"') {
+            tok.push_back('\"');
+            i += 2;
+        }
+        else if (str[i] == '\"' && inside_quotas) {
+            inside_quotas = false;
+            i++;        
+        }
+        else if (str[i] == ',' && !inside_quotas) {
             tokens.push_back(tok);
             tok.clear();
-        } else {
+            i++;
+        } 
+        else {
             tok.push_back(str[i]);
+            i++;
         }
     }
     tokens.push_back(tok);
-    tok.clear();
     return tokens;
 }
 
@@ -37,27 +44,49 @@ struct CSVDataFrame {
     bool has_header;
     int rows, cols;
 
+    // Constructor
     CSVDataFrame(const char *path, bool head = true) {
         std::ifstream fin(path);
         std::string row;
         has_header = head;
         std::getline(fin, row);
         if (head) {
-            header = split(row, ',');
+            header = split(row);
             cols = header.size();
             rows = 0;
         } else {
-            data.push_back(split(row, ','));
+            data.push_back(split(row));
             cols = data[0].size();
             rows = 1;
         }
         while (std::getline(fin, row)) {
-            std::vector<std::string> record = split(row, ',');
+            std::vector<std::string> record = split(row);
             data.push_back(record);
             rows++;
-        }       
+        }
+        fin.close();       
     }
 
+    CSVDataFrame(CSVDataFrame &frm) {
+        rows = frm.rows;
+        cols = frm.cols;
+        has_header = frm.has_header;
+        if (has_header) {
+            header = std::vector<std::string>(cols);
+            for (int i = 0; i < cols; i++) {
+                header[i] = frm.header[i];
+            }
+        }
+        data = std::vector<std::vector<std::string>>(rows);
+        for (int i = 0; i < rows; i++) {
+            data[i] = std::vector<std::string>(cols);
+            for (int j = 0; j < cols; j++) {
+                data[i][j] = frm.data[i][j];
+            }
+        }
+    }
+
+    // Print. Debug function
     void print() {
         if (has_header) {
             for (std::string field : header) {
@@ -72,11 +101,81 @@ struct CSVDataFrame {
             std::cout << std::endl;
         }
     }
+
+    // Get function
+    std::string get(int row, std::string col) {
+        int i = 0;
+        while (header[i] != col) {
+            i++;
+        }
+        return data[row][i];
+    }
+
+    // Change field
+    void change(int row, std::string col, std::string value) {
+        int i = 0;
+        while (header[i] != col) {
+            i++;
+        }
+        data[row][i] = value;
+    }
+
+    // Add record
+    void add_record() {
+        std::vector<std::string> record(cols);
+        data.push_back(record);
+        rows++;
+    }
+
+    void add_record(int pos) {
+        std::vector<std::string> record(cols);
+        data.insert(data.begin() + pos, record);
+        rows++;
+    }
+
+    // Delete record
+    void delete_record(int pos) {
+        data.erase(data.begin() + pos);
+        rows--;
+    }
+
+    // Add column
+    void add_column(int pos, std::string name) {
+        header.emplace(header.begin() + pos, name);
+        cols++;
+        for (int i = 0; i < rows; i++) {
+            data[i].emplace(data[i].begin() + pos, "");
+        }
+    }
+
+    // Delete column
+    void delete_column(std::string name) {
+        int i = 0;
+        while (header[i] != name) {
+            i++;
+        }
+        header.erase(header.begin() + i);
+        cols--;
+        for (int j = 0; j < rows; j++) {
+            data[j].erase(data[j].begin() + i);
+        }
+    }
+
 };
 
 
 int main() {
     CSVDataFrame frame("example.csv");
+    frame.print();
+    std::cout << frame.get(9, "model") << std::endl;
+    frame.add_column(5, "Review");
+    frame.change(0, "Review", "3.5");
+    frame.change(1, "Review", "4.2");
+    frame.change(2, "Review", "4.1");
+    frame.change(3, "Review", "2.1");
+    frame.print();
+    std::cout << std::endl;
+    frame.delete_column("Review");
     frame.print();
     return 0;
 }
